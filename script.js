5cbe83c9433996f5111ed4e7b6c755166f76b8f5
iagosm <iagosousa201486@gmail.com>
2022-10-27 17:54:00 -0300

    Projeto Card NLW Copa

---
+++ script.js
@@ -0,0 +1,12 @@
+//Logica para clicar e mudar o background do card
+
+let isIgnite = false
+
+function changeCard(event){
+   const card = event.currentTarget
+   const bg = isIgnite ? "explore" : "ignite"
+   isIgnite = !isIgnite
+   card.style.backgroundImage = `url(assets/bg-${bg}.svg)`
+
+}
+
